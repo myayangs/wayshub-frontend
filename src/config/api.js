db42230321710c956fe04b919d69a403d5a6ca4b
myyngstwn <myayangs@gmail.com>
2023-04-16 16:00:40 +0000

    stressssss

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1"
+    baseURL: "http://103.37.125.64:5000/api/v1"
 });
 
 const setAuthToken = (token) => {
